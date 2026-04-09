6ef5d2c4355e7090f2381766e77808fee8575ba4
Stephen Paul Lutar JR <stephenlutar2@gmail.com>
2026-04-08 20:10:09 -0400

    Clean up: remove artifacts/api-server/src/middleware/index.ts

--- artifacts/api-server/src/middleware/index.ts
+++
@@ -1,4 +0,0 @@
-export { securityHeaders } from "./securityHeaders";
-export { validateBody, validateQuery } from "./validate";
-export { requireRole, requireAdmin, requireOperator, resolveRole, type Role } from "./rbac";
-export { rateLimit, authRateLimit, writeRateLimit } from "./rateLimit";
